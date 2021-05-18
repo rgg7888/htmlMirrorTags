9a608e75dc386ad4e7e5e05cd96927d8ea239fab
rgg7888 <rgg7888@outlook.com>
2021-05-17 22:02:42 -0600

    modificando los namespaces dentro de los mirror helpers

--- src/helpers.php
+++ src/helpers.php
@@ -9,28 +9,28 @@ if(!function_exists('_nav')) {
 
 if(!function_exists('_div')) {
     function _div(array $atributos = [],$content = null) {
-        $div = new App\tags\div\Div($content,$atributos);
+        $div = new App\tags\mirrorDiv\Div($content,$atributos);
         return $div->add();
     }
 }
 
 if(!function_exists('_a')) {
     function _a(array $atributos = [],$content = null) {
-        $a = new App\tags\a\A($content,$atributos);
+        $a = new App\tags\mirrorA\A($content,$atributos);
         return $a->add();
     }
 }
 
 if(!function_exists('_ul')) {
     function _ul(array $atributos = [],$content = null) {
-        $ul = new App\tags\ul\Ul($content,$atributos);
+        $ul = new App\tags\mirrorUl\Ul($content,$atributos);
         return $ul->add();
     }
 }
 
 if(!function_exists('_li')) {
     function _li(array $atributos = [],$content = null) {
-        $li = new App\tags\li\Li($content,$atributos);
+        $li = new App\tags\mirrorLi\Li($content,$atributos);
         return $li->add();
     }
 }
